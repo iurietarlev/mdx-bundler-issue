1d71cbf5e5cec8a5ef27bbc9c46edcfb7bc087a7
iurie <iurie.tarlev@acresoftware.com>
2022-01-03 22:00:19 +0000

    fix typescript error

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,18 +1,24 @@
 import React from 'react'
-import { getMDXComponent } from 'mdx-bundler/client'
+import { getMDXExport } from 'mdx-bundler/client'
 import { getAllPosts, getSinglePost } from '../../utils/mdx'
-import Link from 'next/link'
+import Link, { LinkProps } from 'next/link'
 
-const CustomLink = ({ as, href, ...otherProps }) => {
+const CustomLink = ({
+  as,
+  href,
+  ...otherProps
+}: Pick<LinkProps, 'as' | 'href'> &
+  React.DetailedHTMLProps<React.AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement>) => {
   return (
-    <Link as={as} href={href} className="custom-link">
+    <Link as={as} href={href}>
       <a {...otherProps} />
     </Link>
   )
 }
 
 const Post = ({ code, frontmatter }: { code: string; frontmatter: { [key: string]: any } }) => {
-  const Component = React.useMemo(() => getMDXComponent(code), [code])
+  // types don't work correctly with getMDXComponent, so use getMDXExport instead
+  const Component = React.useMemo(() => getMDXExport(code).default, [code])
   return (
     <div className="wrapper">
       <h1>{frontmatter.title}</h1>
